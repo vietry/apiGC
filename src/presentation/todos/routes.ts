de9374631b25381f28c75677d194d0eb0ac72ed8
Carlos Vera <vera3000@gmail.com>
2024-01-15 17:35:05 -0500

    video 234 min 2:11 Consumir casos de uso

--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import { TodosController } from "./controller";
+import { TodoDatasourceImpl } from "../../infrastucture/datasource/todo.datasource.impl";
+import { TodoRepositoryImpl } from "../../infrastucture/repositories/todo.repository.impl";
 
 
 export class TodoRoutes {
     static get routes(): Router{
         const router = Router();
-        const todoController = new TodosController();
+
+        const datasource = new TodoDatasourceImpl();
+        const todoRepository = new TodoRepositoryImpl(datasource);
+        const todoController = new TodosController(todoRepository);
 
         router.get('/', todoController.getTodos);
         router.get('/:id', todoController.getTodoById);
